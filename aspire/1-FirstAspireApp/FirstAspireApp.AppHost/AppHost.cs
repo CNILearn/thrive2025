4de7841e42fc6f4309bc9dda46fcb0a4adb4d89c
Christian Nagel <christian@christiannagel.com>
2025-06-03 08:21:49 +0200

    first app with publish azure sql

--- aspire/1-FirstAspireApp/FirstAspireApp.AppHost/AppHost.cs
+++ aspire/1-FirstAspireApp/FirstAspireApp.AppHost/AppHost.cs
@@ -1,13 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sqlDb = builder.AddSqlServer("SqlServer")
-    .WithDataVolume("sqlserver-data")
-    .AddDatabase("mysqldb");
-
 var apiService = builder.AddProject<Projects.FirstAspireApp_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health")
-    .WithReference(sqlDb)
-    .WaitFor(sqlDb);
+    .WithHttpHealthCheck("/health");
+
+if (builder.ExecutionContext.IsPublishMode)
+{
+    // In publish mode, we use Azure SQL Server instead of local SQL Server
+    var sqlDb = builder.AddAzureSqlServer("sqlServer")
+        .AddDatabase("mysqldb");
+
+    apiService.WithReference(sqlDb).WaitFor(sqlDb);
+}
+else
+{
+    var sqlDb = builder.AddSqlServer("sqlserver")
+        .WithDataVolume("sqlserver-data")
+        .AddDatabase("mysqldb");
+
+    apiService.WithReference(sqlDb).WaitFor(sqlDb);
+}
 
 builder.AddProject<Projects.FirstAspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
